d4b4f22eaf071dfbba2b6cdfd1eb47f8db7f17b3
Nicholas Gunner <ngunner@orbitist.com>
2025-04-08 13:31:12 -0400

    Update map integration to use Mapbox GL JS and adjust style settings for improved satellite view

--- Sim/js/app.js
+++ Sim/js/app.js
@@ -55,30 +55,9 @@ createApp({
 
     mounted() {
         // Initialize map
-        this.map = new maplibregl.Map({
+        this.map = new mapboxgl.Map({
             container: 'map',
-            style: {
-                version: 8,
-                sources: {
-                    'satellite': {
-                        type: 'raster',
-                        tiles: [
-                            'https://api.maptiler.com/tiles/satellite/{z}/{x}/{y}.jpg?key=get_your_own_D6rA4zTHduk6KOKTXzGB'
-                        ],
-                        tileSize: 256,
-                        attribution: '© MapTiler © OpenStreetMap contributors'
-                    }
-                },
-                layers: [
-                    {
-                        id: 'satellite',
-                        type: 'raster',
-                        source: 'satellite',
-                        minzoom: 0,
-                        maxzoom: 22
-                    }
-                ]
-            },
+            style: 'mapbox://styles/mapbox/satellite-v9',
             center: [-75.6, 42.9],  // NY state center
             zoom: 6,
             accessToken: 'pk.eyJ1IjoibnJnNDIiLCJhIjoiY205OHJmYWNjMDY0ajJrb2JqazQ5cWw4NiJ9.GDAeRs3uwE8Y-WaqLyFomw'
